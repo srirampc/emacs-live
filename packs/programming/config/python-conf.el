0fd2c794201e6dda661033fc7bfcc90f5e880efd
srirampc <sriram.pc@gmail.com>
2012-10-13 01:00:23 +0530

    Update python configuration for emacs-ipython-notebook

--- packs/programming/config/python-conf.el
+++ packs/programming/config/python-conf.el
@@ -1,7 +1,16 @@
 (live-add-pack-lib "python-mode")
-(live-add-pack-lib "ipython")
 (live-add-pack-lib "anything-python")
+(live-add-pack-lib "websocket")
+(live-add-pack-lib "popup")
+(live-add-pack-lib "fuzzy")
+(live-add-pack-lib "python")
+(live-add-pack-lib "smartrep")
+(live-add-pack-lib "nxhtml")
+(live-add-pack-lib "ert")
+(live-add-pack-lib "pos-tip")
+(live-add-pack-lib "emacs-ipython-notebook/lisp")
 
+;;(live-add-pack-lib "ipython")
 ;;(require 'ipython)
 ;;(setq-default py-python-command-args '("--pylab" "--colors=NoColor"))
 ;; ;; TODO: update auto-complete thingy
@@ -17,12 +26,21 @@
 ;;                 'anything-ipython-complete)))
 
 
+;; pymacs - use more ropemacs
 (live-add-pack-lib "pymacs")
 (require 'pymacs)
 (pymacs-load "ropemacs" "rope-")
 (setq ropemacs-enable-autoimport t)
-(require 'auto-complete-config)
- ;;(add-to-list 'ac-dictionary-directories "~/.emacs.d/vendor/auto-complete-1.2/dict")
-(ac-config-default)
 
-(require 'ipython)
+;; ein
+(require 'ein)
+;; auto-complete
+(setq ein:use-auto-complete-superpack t)
+(require 'auto-complete-config nil t)
+
+(when (featurep 'auto-complete-config)
+  (ac-config-default)
+  ;; (add-to-list 'ac-dictionary-directories
+  ;;              (concat (live-pack-lib-dir) "auto-complete/dict"))
+  (global-auto-complete-mode t))
+(require 'ein-dev)
