553ca16ab8cf48bc6bc7bb74900fac5060062591
Sriram P C <sriram.pc@gmail.com>
2012-10-13 17:45:03 +0530

    Updated ESS to latest version

--- packs/programming/lib/ess/ess-autoloads.el
+++ packs/programming/lib/ess/ess-autoloads.el
@@ -1,79 +1,76 @@
-;;; ess-autoloads.el --- automatically extracted autoloads
+;;; ess-autoloads.el --- Emacs Speaks Statistics: statistical programming within Emacs
+
+;; Copyright (C) 2012 Donald E. Curtis
+
+;; Original Author: Donald E. Curtis
+;; Created: March 5, 2012
+;; Maintainers: ESS-core <ESS-core@r-project.org>
+
+;; Keywords: autoload, startup, configuration, package.el
+;; Summary: essential autoloads for ESS
+
+;; This file is part of ESS
+
+;; This file is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 2, or (at your option)
+;; any later version.
 ;;
+;; This file is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.	 See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs; see the file COPYING.	If not, write to
+;; the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.
+;;
+;; In short: you may use this code any way you like, as long as you
+;; don't charge money for it, remove this notice, or hold anyone liable
+;; for its results.
+
+;;; Commentary:
+
+;;; This file is just a wrapper so that ESS will be more `package.el'
+;;; friendly.  This is just a list of functions that require loading
+;;; of ESS through `ess-site'.
+
 ;;; Code:
 
-
-;;;### (autoloads (Rd-mode) "ess-rd" "ess-rd.el" (20410 16559))
-;;; Generated autoloads from ess-rd.el
+;;*;; Requires and autoloads
+;;;=====================================================
+;;;
 
-(autoload 'Rd-mode "ess-rd" "\
-Major mode for editing R documentation source files.
+(when (boundp 'load-path)
+  (add-to-list 'load-path
+               (file-name-as-directory
+                (expand-file-name
+                 "lisp"
+                 (file-name-directory load-file-name)))))
 
-This mode makes it easier to write R documentation by helping with
-indentation, doing some of the typing for you (with Abbrev mode) and by
-showing keywords, strings, etc. in different faces (with Font Lock mode
-on terminals that support it).
+(autoload 'R "ess-site" "Call 'R', the 'GNU S' system from the R Foundation.
+Optional prefix (C-u) allows to set command line arguments, such as
+--vsize.  This should be OS agnostic.
+If you have certain command line arguments that should always be passed
+to R, put them in the variable `inferior-R-args'." t)
 
-Type \\[list-abbrevs] to display the built-in abbrevs for Rd keywords.
+(autoload 'S "ess-site" "Call 'S'." t)
 
-Keybindings
-===========
+ ; Local variables section
 
-\\{Rd-mode-map}
+;;; This file is automatically placed in Outline minor mode.
+;;; The file is structured as follows:
+;;; Chapters:	  ^L ;
+;;; Sections:	 ;;*;;
+;;; Subsections: ;;;*;;;
+;;; Components:	 defuns, defvars, defconsts
+;;;		 Random code beginning with a ;;;;* comment
 
-Variables you can use to customize Rd mode
-==========================================
+;;; Local variables:
+;;; mode: emacs-lisp
+;;; mode: outline-minor
+;;; outline-regexp: "\^L\\|\\`;\\|;;\\*\\|;;;\\*\\|(def[cvu]\\|(setq\\|;;;;\\*"
+;;; End:
 
-`Rd-indent-level'
-  Indentation of Rd code with respect to containing blocks.
-  Default is 2.
-
-Turning on Rd mode runs the hook `Rd-mode-hook'.
-
-To automatically turn on the abbrev(iate) features, add the
-following lines to your `.emacs' file:
-
-  (add-hook 'Rd-mode-hook
-	    (lambda ()
-	      (abbrev-mode 1)))
-
-\(fn)" t nil)
-
-;;;***
-
-;;;### (autoloads (mouse-me) "mouseme" "mouseme.el" (20410 16558))
-;;; Generated autoloads from mouseme.el
-
-(autoload 'mouse-me "mouseme" "\
-Popup a menu of functions to run on selected string or region.
-
-\(fn EVENT)" t nil)
-
-;;;***
-
-;;;### (autoloads nil nil ("ess-arc-d.el" "ess-bugs-d.el" "ess-bugs-l.el"
-;;;;;;  "ess-comp.el" "ess-compat.el" "ess-custom.el" "ess-dde.el"
-;;;;;;  "ess-debug.el" "ess-eldoc.el" "ess-font-lock.el" "ess-help.el"
-;;;;;;  "ess-inf.el" "ess-install.el" "ess-jags-d.el" "ess-lsp-l.el"
-;;;;;;  "ess-menu.el" "ess-mode.el" "ess-mous.el" "ess-mouse.el"
-;;;;;;  "ess-noweb.el" "ess-omg-d.el" "ess-omg-l.el" "ess-pkg.el"
-;;;;;;  "ess-r-a.el" "ess-r-args.el" "ess-r-d.el" "ess-r-gui.el"
-;;;;;;  "ess-rdired.el" "ess-roxy.el" "ess-rutils.el" "ess-s-l.el"
-;;;;;;  "ess-s3-d.el" "ess-s4-d.el" "ess-sas-a.el" "ess-sas-d.el"
-;;;;;;  "ess-sas-l.el" "ess-send.el" "ess-send2.el" "ess-site.el"
-;;;;;;  "ess-sp3-d.el" "ess-sp4-d.el" "ess-sp5-d.el" "ess-sp6-d.el"
-;;;;;;  "ess-sp6w-d.el" "ess-sta-d.el" "ess-sta-l.el" "ess-swv.el"
-;;;;;;  "ess-toolbar.el" "ess-trns.el" "ess-utils.el" "ess-vst-d.el"
-;;;;;;  "ess-xls-d.el" "ess.el" "essd-els.el" "make-regexp.el" "msdos.el"
-;;;;;;  "noweb-font-lock-mode.el" "noweb-mode.el") (20410 16560 621267))
-
-;;;***
-
-(provide 'ess-autoloads)
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; coding: utf-8
-;; End:
 ;;; ess-autoloads.el ends here
+
