eb3dbcf46049a46cd0850b4610468694b0bc2b28
srirampc <sriram.pc@gmail.com>
2013-01-18 19:06:44 +0530

    Set font only for gnu/linux

--- packs/power/config/theme-conf.el
+++ packs/power/config/theme-conf.el
@@ -1,6 +1,8 @@
 ;;
-(set-default-font
-   "-outline-Inconsolata-normal-r-normal-normal-17-97-96-96-c-*-iso10646-1")
+(if (equalp (symbol-name system-type) "gnu/linux")
+    (set-default-font
+     "-outline-Inconsolata-normal-r-normal-normal-16-97-96-96-c-*-iso10646-1"))
+
 ;;
 (setq custom-theme-directory (live-pack-lib-dir))
-(load-theme 'blackboard t)
+;;(load-theme 'blackboard t)
